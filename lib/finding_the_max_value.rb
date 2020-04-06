af1803780624f66390eda3072877f41025ee62b3
James Stamps Jr <jstampshere@gmail.com>
2020-04-06 14:07:31 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,12 +1,16 @@
 
 def find_max_value(array)
-  count = 0
+  counter = 0
   max_value = -1
-  while count < array.length do
-    if max_value < array[count]
-      max_value = array[count]
-    end
-    count += 1
-  end
-  max_value
-end
+   while count < array.length 
+   do
+     if max_value < array[count]
+       max_value = array[count]
+     end
+     count += 1
+   end
+   max_value
+ end
+ 
+ 
+ 
